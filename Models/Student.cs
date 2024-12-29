6a06685252bb711e9dde41154b5780478ec2fa1a
VAMSHI-4 <vamshikrishnaryagalla3@gmail.com>
2024-12-29 07:48:29 +0530

    Add files via upload

---
+++ Models/Student.cs
@@ -0,0 +1,15 @@
+﻿namespace StudentManagementAPI.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string? Email { get; set; }
+
+        public string? Course { get; set; }
+
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
---
+++ Models/StudentDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudentManagementAPI.Models
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options) { }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
